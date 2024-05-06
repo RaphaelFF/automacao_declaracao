9019416fd03077f6ca71cbdf6b574411667d7aeb
Raphael Fernandes Franca <116182607+RaphaelFF@users.noreply.github.com>
2024-05-06 20:10:23 -0300

    marcacao

--- man_word/app.py
+++ man_word/app.py
@@ -1,23 +1,13 @@
 from docx import Document
 
 nome_arquivo = './teste.docx'  # Nome do arquivo Word
-texto_antigo = 'nome_'  # Texto que deve ser substituído
+texto_antigo = 'teste'  # Texto que deve ser substituído
 texto_novo = 'Raphael Fernanades Franca com '  # Novo texto para substituir
 
 documento = Document(nome_arquivo) #abrindo o arquivo
 
-# Substitui o texto antigo pelo novo em todo o parágrafo
 
-'''
-for p in range(10):
-    paragrafo = documento.paragraphs[p]  # Assumindo que o texto está no primeiro parágrafo
-
-    paragrafo.text = paragrafo.text.replace(texto_antigo, texto_novo)
-
-    # Salva o documento modificado
-    
-'''
-
+#percorrendo o documento inteiro até achar a palavra para ser substituida
 for paragrafo in documento.paragraphs:
     for linha in paragrafo.runs:
         if texto_antigo in linha.text:
